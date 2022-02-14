cd3ea69616dc7aecbf19ee08c809eb18895fe217
Jehad91 <j.abushaqra1@gmail.com>
2022-02-14 10:54:38 +0200

    exercies done

---
+++ stage-1/exercise/app.js
@@ -0,0 +1,121 @@
+/*
+ * Exercise: Refactor the code!
+ *
+ * This script file contains a small front-end app that queries the
+ * StackOverflow API. It works, but the code is not ideal; there is a lot of
+ * work to do to clean it up.
+ *
+ * First take a few minutes to understand what the code is doing, then use what
+ * you have learned in the preceding stage-1 exercises to refactor the app.
+ *
+ * Take your time, and think about what principles you are trying to apply while
+ * you are refactoring.
+ */
+'use strict';
+
+const addListener = (selector, eventName, callback) => {
+  document.querySelector(selector).addEventListener(eventName, callback);
+};
+
+const fetch = (url, callback) => {
+  let xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", function () {
+      if (xhr.status === 200) {
+          let response = JSON.parse(xhr.responseText);
+          return callback(response);
+      } else {
+          console.log("Status Code: " + xhr.status);
+      }
+  });
+
+  xhr.open("GET", url);
+  xhr.send();
+};
+
+const renderSummaryDom = (tags,responseLength)=>{
+  document.querySelector("#results-summary").innerHTML =
+          "" +
+          "<p>" +
+          "Query of " +
+          tags +
+          " returned " +
+          responseLength +
+          " results" +
+          "</p>";
+}
+
+const renderResultDom = (items,callback) => {
+  document.querySelector("#results-body").innerHTML = items.map(callback).join("<br>");
+}
+
+
+addListener("#form-unanswered", "submit", (event) => {
+  event.preventDefault();
+
+  let form = event.target;
+  let tags = form.querySelector("input[name=tags]").value;
+  let url =
+      "https://api.stackexchange.com/2.2/questions/unanswered?order=desc&sort=activity&site=stackoverflow&tagged=" +
+      tags;
+
+  fetch(url, function (response) {
+      renderSummaryDom(tags,response.items.length);
+
+      renderResultDom(response.items,(item)=>{
+          return (
+              "" +
+              "<div>" +
+              "<p>Title: " +
+              item.title +
+              "</p>" +
+              "<p>Date: " +
+              new Date(item.creation_date) +
+              "</p>" +
+              '<p>Link: <a href="' +
+              item.link +
+              '">Click here</a></p>' +
+              "<p>Owner: " +
+              item.owner.display_name +
+              "</p>" +
+              "</div>"
+          );
+      });
+
+  });
+});
+
+addListener("#form-answerers", "submit", (event) => {
+  event.preventDefault();
+
+  let form = event.target;
+  let tag = form.querySelector("input[name=tags]").value;
+  let url =
+      "http://api.stackexchange.com/2.2/tags/" +
+      tag +
+      "/top-answerers/all_time?site=stackoverflow";
+
+  fetch(url, function (response) {
+      renderSummaryDom(tag,response.items.length);
+
+      renderResultDom(response.items,(item)=>{
+          return (
+              "" +
+              "<div>" +
+              "<p>User: " +
+              item.user.display_name +
+              "</p>" +
+              "<p>Reputation: " +
+              item.user.reputation +
+              "</p>" +
+              '<p>Profile: <a href="' +
+              item.user.link +
+              '">Click here</a></p>' +
+              "<p>Score: " +
+              item.score +
+              "</p>" +
+              "</div>"
+          );
+      });
+  });
+});
---
+++ stage-2/module-pattern/exercise.js
@@ -0,0 +1,115 @@
+/*
+ * Exercise: Create some modules!
+ *
+ * When you think you have finished, run the command:
+ *   npm run s2.modules
+ * This will run a series of tests which should all pass.
+ */
+'use strict';
+
+/*
+ * Create a single module (using an IIFE) which contains functionality to parse
+ * URLs.
+ *
+ * We have started you off with the basic structure.
+ *
+ *     https    ://   www.example.com  /   hello  ?  foo=1&bar=2
+ * |          |     |                |   |      |  |             |
+ * | protocol |     |    domain      |   | path |  | querystring |
+ */
+let UrlParser = (function () {
+  let urlRegEx = /^([a-z]+):\/\/([a-z\.]+)\/([^\?]+)\?(.+)$/i;
+
+  function getMatch (string) {
+    return string.match(urlRegEx);
+  }
+
+  function numberBasicStructure (n, array) {
+    return (array || [])[n];
+  }
+
+  return {
+    // a function that takes a URL and returns its protocol
+    protocol: function (url) {
+      return numberBasicStructure(1, getMatch(url));
+    },
+
+    // a function that takes a URL and returns its domain
+    domain: function (url) {
+      return numberBasicStructure(2, getMatch(url));
+    },
+
+    // a function that takes a URL and returns its path
+    path: function (url) {
+      return numberBasicStructure(3, getMatch(url));
+    },
+
+    // a function that takes a URL and returns its query string
+    querystring: function (url) {
+      return numberBasicStructure(4, getMatch(url));
+    },
+  };
+})();
+
+/*
+ * Create a module that can support multiple instances (like in our example).
+ * The module should be a function with several additional methods attached as
+ * attributes.
+ *
+ * Example:
+ * var exampleBuilder = createUrlBuilder('https://example.com');
+ *
+ * var url = exampleBuilder({ query: { foo: 1, bar: 2 }, path: 'hello' });
+ *
+ * console.log(url); // https://example.com/hello?foo=1&bar=2
+ *
+ * exampleBuilder.
+ */
+let createUrlBuilder = function (host) {
+
+  function queryObjectToString (query) {
+    return Object.keys(query)
+      .map(function (key) {
+        return key + '=' + query[key];
+      })
+      .join('&');
+  }
+
+  function appendPath (base, path) {
+    return base + '/' + path;
+  }
+
+  function appendQueryString (base, query) {
+    return base + '?' + queryObjectToString(query);
+  }
+
+  let builder = function (config) {
+    let url = host;
+
+    if (config.path) {
+      url = appendPath(url, config.path);
+    }
+
+    if (config.query) {
+      url = appendQueryString(url, config.query);
+    }
+
+    return url;
+  };
+
+  builder.path = function (path) {
+    return appendPath(host, path);
+  };
+
+  builder.query = function (query) {
+    return appendQueryString(host, query);
+  };
+
+  return builder;
+};
+
+
+module.exports = {
+  UrlParser,
+  createUrlBuilder,
+};
